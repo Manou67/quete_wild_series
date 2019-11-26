629f2b1562b88eabb5a5e6dff40cb2ba0e7fb2d2
Manou67 <manou.buono@gmail.com>
2019-11-26 22:44:33 +0100

    quete 05 - routing avancé

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -12,9 +12,10 @@ class DefaultController extends AbstractController
     /**
      * @Route("/", name="app_index")
      */
+
     public function index() :Response
     {
-        return $this->render('home.html.twig');
+        return $this->render('home/home.html.twig');
     }
 }
 
